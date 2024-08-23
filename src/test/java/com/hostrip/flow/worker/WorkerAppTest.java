a59026d47a80830570b1b290b663cbda0f30d99a
Eleven <idiotalex@163.com>
2024-08-23 23:24:15 +0800

    chore: init project

---
+++ src/test/java/com/hostrip/flow/worker/WorkerAppTest.java
@@ -0,0 +1,16 @@
+package com.hostrip.flow.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class WorkerAppTest {
+
+    @Test
+    public void test() {
+        log.info("test");
+    }
+}
