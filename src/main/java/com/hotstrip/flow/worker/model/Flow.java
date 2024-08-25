548e05a22883a2639b27413a62fe842f8419dce7
Eleven <idiotalex@163.com>
2024-08-25 13:07:35 +0800

    chore: update flow table columns

--- src/main/java/com/hotstrip/flow/worker/model/Flow.java
+++ src/main/java/com/hotstrip/flow/worker/model/Flow.java
@@ -1,9 +1,14 @@
 package com.hotstrip.flow.worker.model;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class Flow {
   private Long id;
   private String name;
+  private String jsonData;
+  private Date createdAt;
+  private Date updatedAt;
 }
