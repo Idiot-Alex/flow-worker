8bd433038996eedf303fdcd3f44023ab4c0af8cf
idiotalex <idiotalex@163.com>
2024-09-09 10:14:22 +0800

    fix: api and config file

--- src/main/java/com/hotstrip/flow/worker/mapper/FlowMapper.java
+++ src/main/java/com/hotstrip/flow/worker/mapper/FlowMapper.java
@@ -1,6 +1,9 @@
 package com.hotstrip.flow.worker.mapper;
 
 import io.mybatis.mapper.BaseMapper;
+
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import com.hotstrip.flow.worker.model.Flow;
@@ -11,9 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public interface FlowMapper extends BaseMapper<Flow, Long> {
 
+    @Deprecated
     @Insert("INSERT INTO flow (id, name, json_data) VALUES (#{id}, #{name}, #{jsonData})")
     int insertFlow(Flow flow);
 
     int updateById(Flow entity);
+
+    List<Flow> list(Flow entity);
 
 }
